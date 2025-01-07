303de27f23682b7420114f789ab65722af2a658b
Nguyen Trung Kien <103893111+kiennguyen1120@users.noreply.github.com>
2025-01-07 16:13:01 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'ap-northeast-1_i32TpS7C6', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '1tl26kj5jjbphd56pnqek9cmts', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '44pmst78chuirn9duph9epqch', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-northeast-1' // e.g. us-east-2
     },
     api: {
